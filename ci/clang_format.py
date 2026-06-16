4c8726d0d7f013e6e8fca244427e71d1c65ac536
Matthew Fernandez <matthew.fernandez@gmail.com>
2026-06-15 17:20:31 -0700

    sparse: clang-format DotIO.h

--- ci/clang_format.py
+++ ci/clang_format.py
@@ -411,7 +411,6 @@ EXCLUDE = (
     "lib/sfio/sftable.c",
     "lib/sfio/sfprint.c",
     "lib/sfio/sfvscanf.c",
-    "lib/sparse/DotIO.h",
     "lib/sparse/QuadTree.c",
     "lib/sparse/QuadTree.h",
     "lib/sparse/SparseMatrix.c",
