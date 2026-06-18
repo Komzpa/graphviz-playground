543cf68bbc0868fd3848e7bb745954b35155441d
Matthew Fernandez <matthew.fernandez@gmail.com>
2026-06-17 17:01:16 -0700

    gvmap: calculate norms with 'size_t' values
    This pays down some debt from previous changes.

--- cmd/gvmap/country_graph_coloring.c
+++ cmd/gvmap/country_graph_coloring.c
@@ -17,29 +17,31 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <time.h>
+#include <util/gv_math.h>
+#include <util/prisize_t.h>
 
-static double get_local_12_norm(int n, int i, const int *ia, const int *ja,
-                                const int *p) {
-  double norm = n;
+static size_t get_local_12_norm(size_t n, size_t i, const int *ia,
+                                const int *ja, const int *p) {
+  size_t norm = n;
   for (int j = ia[i]; j < ia[i+1]; j++){
-    if (ja[j] == i) continue;
-    norm = fmin(norm, abs(p[i] - p[ja[j]]));
+    if (ja[j] >= 0 && (size_t)ja[j] == i) continue;
+    norm = zmin(norm, (size_t)abs(p[i] - p[ja[j]]));
   }
   return norm;
 }
 
-static void get_12_norm(int n, const int *ia, const int *ja, int *p,
-                        double *norm) {
+static void get_12_norm(size_t n, const int *ia, const int *ja, int *p,
+                        size_t *norm) {
   /* norm[0] := antibandwidth
      norm[1] := (\sum_{i\in V} (Min_{{j,i}\in E} |p[i] - p[j]|)/|V|
   */
   norm[0] = n; norm[1] = 0;
-  for (int i = 0; i < n; i++){
-    double tmp = n;
+  for (size_t i = 0; i < n; i++){
+    size_t tmp = n;
     for (int j = ia[i]; j < ia[i+1]; j++){
-      if (ja[j] == i) continue;
-      norm[0] = fmin(norm[0], abs(p[i] - p[ja[j]]));
-      tmp = fmin(tmp, abs(p[i] - p[ja[j]]));
+      if (ja[j] >= 0 && (size_t)ja[j] == i) continue;
+      norm[0] = zmin(norm[0], (size_t)abs(p[i] - p[ja[j]]));
+      tmp = zmin(tmp, (size_t)abs(p[i] - p[ja[j]]));
     }
     norm[1] += tmp;
   }
@@ -49,7 +51,7 @@ static void get_12_norm(int n, const int *ia, const int *ja, int *p,
 void improve_antibandwidth_by_swapping(SparseMatrix A, int *p){
   int cnt = 1, *ia = A->ia, *ja = A->ja;
   const size_t n = A->m;
-  double norm1[2];
+  size_t norm1[2];
   clock_t start = clock();
   FILE *fp = NULL;
   
@@ -61,17 +63,17 @@ void improve_antibandwidth_by_swapping(SparseMatrix A, int *p){
   for (bool improved = true; improved; ) {
     improved = false;
     for (size_t i = 0; i < n; i++) {
-      norm1[0] = get_local_12_norm((int)n, (int)i, ia, ja, p);
+      norm1[0] = get_local_12_norm(n, i, ia, ja, p);
       for (size_t j = 0; j < n; j++) {
 	if (j == i) continue;
-	const double norm2 = get_local_12_norm((int)n, (int)j, ia, ja, p);
+	const size_t norm2 = get_local_12_norm(n, j, ia, ja, p);
 	const int pi = p[i];
 	const int pj = p[j];
 	p[i] = pj;
 	p[j] = pi;
-	const double norm11 = get_local_12_norm((int)n, (int)i, ia, ja, p);
-	const double norm22 = get_local_12_norm((int)n, (int)j, ia, ja, p);
-	if (fmin(norm11, norm22) > fmin(norm1[0], norm2)){
+	const size_t norm11 = get_local_12_norm(n, i, ia, ja, p);
+	const size_t norm22 = get_local_12_norm(n, j, ia, ja, p);
+	if (zmin(norm11, norm22) > zmin(norm1[0], norm2)){
 	  improved = true;
 	  norm1[0] = norm11;
 	  continue;
@@ -80,15 +82,15 @@ void improve_antibandwidth_by_swapping(SparseMatrix A, int *p){
 	p[j] = pj;
       }
       if (i%100 == 0 && Verbose) {
-	get_12_norm((int)n, ia, ja, p, norm1);
-	fprintf(fp, "%f %f %f\n", ((double)(clock() - start)) / CLOCKS_PER_SEC,
+	get_12_norm(n, ia, ja, p, norm1);
+	fprintf(fp, "%f %" PRISIZE_T " %" PRISIZE_T "\n", (double)(clock() - start) / CLOCKS_PER_SEC,
 	        norm1[0], norm1[1]);
       }
     }
     if (Verbose) {
-      get_12_norm((int)n, ia, ja, p, norm1);
-      fprintf(stderr, "[%d] aband = %f, aband_avg = %f\n", cnt++, norm1[0], norm1[1]);
-      fprintf(fp,"%f %f %f\n", ((double)(clock() - start)) / CLOCKS_PER_SEC,
+      get_12_norm(n, ia, ja, p, norm1);
+      fprintf(stderr, "[%d] aband = %" PRISIZE_T ", aband_avg = %" PRISIZE_T "\n", cnt++, norm1[0], norm1[1]);
+      fprintf(fp,"%f %" PRISIZE_T " %" PRISIZE_T "\n", (double)(clock() - start) / CLOCKS_PER_SEC,
               norm1[0], norm1[1]);
     }
   }
