a4133148a184c44a033cdf005eadadb6b34116c1
Matthew Fernandez <matthew.fernandez@gmail.com>
2026-06-30 23:03:20 +1000

    CI: upgrade macOS 14 (Sonoma) Xcode 15 → 15 (Sequoia) Xcode 16
    The `macos-14-xcode-15` image is no longer available.¹
    
    ¹ https://gitlab.com/gitlab-org/gitlab/-/commit/6694b2d1ebf4e6df380d3677c68e3f8dc90a1e88
    
    Gitlab: closes #2840

--- ci/deploy.py
+++ ci/deploy.py
@@ -217,8 +217,8 @@ def main() -> int:
         "ubuntu-24.04-cmake-build": [f"ubuntu_24.04_graphviz-{version}-cmake.deb"],
         "ubuntu-25.10-cmake-build": [f"ubuntu_25.10_graphviz-{version}-cmake.deb"],
         "ubuntu-26.04-cmake-build": [f"ubuntu_26.04_graphviz-{version}-cmake.deb"],
-        "macos-autotools-build": [f"Darwin_23.6.0_graphviz-{version}-arm64.pkg"],
-        "macos-cmake-build": [f"Darwin_23.6.0_Graphviz-{version}-Darwin.zip"],
+        "macos-autotools-build": [f"Darwin_24.6.0_graphviz-{version}-arm64.pkg"],
+        "macos-cmake-build": [f"Darwin_24.6.0_Graphviz-{version}-Darwin.zip"],
         "windows-cmake-Win32-release-build": [
             f"windows_10_cmake_Release_graphviz-install-{version}-win32.exe",
             f"windows_10_cmake_Release_Graphviz-{version}-win32.zip",
