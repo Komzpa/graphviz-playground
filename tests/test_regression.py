1da4de80665de7223b1cd690a29417c0774dfaa1
Darafei Praliaskouski <me@komzpa.net>
2026-07-13 21:51:48 +0400

    dotgen: drop malformed adjacent flat splines

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -6351,6 +6351,25 @@ def test_2778():
             raise
 
 
+def test_2781():
+    """
+    Graphviz should not crash when processing this graph
+    https://gitlab.com/graphviz/graphviz/-/issues/2781
+    """
+
+    # locate our associated test case in this directory
+    src = Path(__file__).parent / "2781.dot"
+    assert src.exists(), "unexpectedly missing test case"
+
+    # run this through Graphviz
+    try:
+        dot("dot", src)
+    except subprocess.CalledProcessError as e:
+        # allow failure; only fail this test case on a crash
+        if e.returncode != 1:
+            raise
+
+
 def test_2782():
     """
     Graphviz should not crash when processing this graph
