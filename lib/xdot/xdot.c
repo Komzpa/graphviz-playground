8c1ee20935be1d042a654a8ea866094897c486bf
Matthew Fernandez <matthew.fernandez@gmail.com>
2026-08-02 16:32:48 +1000

    xdot linGradient: remove use of intermediate 'd'
    This now sets `stops[i].frac` even in the case of failure. But this does
    not matter as the `CHK1` macro call frees `stops`.

--- lib/xdot/xdot.c
+++ lib/xdot/xdot.c
@@ -896,7 +896,6 @@ static char *radGradient(char *cp, xdot_color *clr) {
  */
 static char *linGradient(char *cp, xdot_color *clr) {
   char *s = cp;
-  double d;
   xdot_color_stop *stops = NULL;
 
   clr->type = xd_linear;
@@ -913,9 +912,8 @@ static char *linGradient(char *cp, xdot_color *clr) {
 
   stops = gv_calloc(clr->u.ling.n_stops, sizeof(stops[0]));
   for (int i = 0; i < clr->u.ling.n_stops; i++) {
-    s = parseReal(s, &d);
+    s = parseReal(s, &stops[i].frac);
     CHK1(s);
-    stops[i].frac = d;
     s = parseString(s, &stops[i].color);
     CHK1(s);
   }
