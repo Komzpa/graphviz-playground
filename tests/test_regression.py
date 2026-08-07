d66fd37f60c356075964f2e6aa9d6865271c43b9
Matthew Fernandez <matthew.fernandez@gmail.com>
2026-08-07 00:51:52 -0700

    add a test demonstrating a 'topsort' crash
    Gitlab: #2851
    Reported-by: Boris Jockov

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -6582,6 +6582,25 @@ def test_2849():
     run_raw(gvpr_bin, "-f", program, graph, timeout=10)
 
 
+@pytest.mark.xfail(
+    raises=subprocess.CalledProcessError,
+    reason="https://gitlab.com/graphviz/graphviz/-/issues/2851",
+    strict=not is_ndebug_defined(),
+)
+def test_2851():
+    """
+    Graphviz should handle `topsort` situations without crashing
+    https://gitlab.com/graphviz/graphviz/-/work_items/2851
+    """
+
+    # locate our associated test case in this directory
+    src = Path(__file__).parent / "2851.dot"
+    assert src.exists(), "unexpectedly missing test case"
+
+    # run this through Graphviz
+    dot("dot", src)
+
+
 def test_698066():
     """
     Graphviz should not crash when processing this graph
