94f4eeaa98026297185aa5d27da95ff12272477d
Matthew Fernandez <matthew.fernandez@gmail.com>
2026-08-09 06:22:00 +1000

    sfdpgen spring_electrical_embedding_fast: remove dead 'qt == NULL' paths
    Coverity observes:
    
      lib/sfdpgen/spring_electrical.c:314:
        deref_ptr_in_call: Dereferencing pointer qt.[show details]
        QuadTree_get_repulsive_force(qt, force, x, bh, p, KP, counts);
        …
      lib/sfdpgen/spring_electrical.c:347:
        CID 1681934: (#1 of 1): Dereference before null check
          (REVERSE_INULL)
        check_after_deref: Null-checking qt suggests that it may be null,
          but it has already been dereferenced on all paths leading to the
          check.
        if (qt) {
    
    Coverity: https://scan5.scan.coverity.com/#/project-view/66357/10724?selectedIssue=1681934
    Reported-by: Coverity 2024.12.1
    Thanks-to: Giuseppe D'Angelo

--- lib/sfdpgen/spring_electrical.c
+++ lib/sfdpgen/spring_electrical.c
@@ -344,28 +344,17 @@ static void spring_electrical_embedding_fast(int dim, SparseMatrix A0,
       for (k = 0; k < dim; k++) x[i*dim+k] += step*f[k];
     }/* done vertex i */
 
-
-
-    if (qt) {
 #ifdef TIME
-      start = clock();
+    start = clock();
 #endif
-      QuadTree_delete(qt);
+    QuadTree_delete(qt);
 #ifdef TIME
-      end = clock();
-      qtree_new_cpu += (double)(end - start) / CLOCKS_PER_SEC;
+    end = clock();
+    qtree_new_cpu += (double)(end - start) / CLOCKS_PER_SEC;
 #endif
 
-      oned_optimizer_train(&qtree_level_optimizer,
-                           counts[0] + 0.85 * counts[1] + 3.3 * counts[2]);
-    } else {
-      if (Verbose) {
-        fprintf(stderr,
-                "\r                iter = %d, step = %f Fnorm = %f nz = %"
-                PRISIZE_T "  K = %f                                  ", iter,
-                step, Fnorm, A->nz, K);
-      }
-    }
+    oned_optimizer_train(&qtree_level_optimizer,
+                         counts[0] + 0.85 * counts[1] + 3.3 * counts[2]);
 
     step = update_step(adaptive_cooling, step, Fnorm, Fnorm0);
   } while (step > tol && iter < maxiter);
