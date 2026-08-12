25b55677f0db445a1c1b699f784c03ce3f5647b8
Matthew Fernandez <matthew.fernandez@gmail.com>
2026-08-12 19:58:18 +1000

    neatogen constrainX: take 'nnodes' as a 'size_t'
    This pays down some debt from a previous commit.

--- lib/neatogen/constraint.c
+++ lib/neatogen/constraint.c
@@ -377,15 +377,14 @@ static void closeGraph(graph_t * cg)
  * (absolute values rather than squares), so we can reuse network simplex.
  * The constraints are encoded as a dag with edges having a minimum length.
  */
-static void constrainX(graph_t* g, nitem* nlist, int nnodes, intersectfn ifn,
+static void constrainX(graph_t *g, nitem *nlist, size_t nnodes, intersectfn ifn,
                        int ortho)
 {
     Dt_t *list = dtopen(&constr, Dtobag);
     nitem *p = nlist;
     graph_t *cg;
-    int i;
 
-    for (i = 0; i < nnodes; i++) {
+    for (size_t i = 0; i < nnodes; i++) {
 	p->val = p->pos.x;
 	dtinsert(list, p);
 	p++;
@@ -397,7 +396,7 @@ static void constrainX(graph_t* g, nitem* nlist, int nnodes, intersectfn ifn,
     rank(cg, 2, INT_MAX);
 
     p = nlist;
-    for (i = 0; i < nnodes; i++) {
+    for (size_t i = 0; i < nnodes; i++) {
 	int newpos, oldpos, delta;
 	oldpos = p->pos.x;
 	newpos = ND_rank(p->cnode);
@@ -554,36 +553,36 @@ int cAdjust(graph_t * g, int mode)
 
 	switch ((adjust_mode)mode) {
 	case AM_ORTHOXY:
-	    constrainX(g, nlist, (int)nnodes, intersectY, 1);
+	    constrainX(g, nlist, nnodes, intersectY, 1);
 	    constrainY(g, nlist, (int)nnodes, intersectX, 1);
 	    break;
 	case AM_ORTHOYX:
 	    constrainY(g, nlist, (int)nnodes, intersectX, 1);
-	    constrainX(g, nlist, (int)nnodes, intersectY, 1);
+	    constrainX(g, nlist, nnodes, intersectY, 1);
 	    break;
 	case AM_ORTHO :
-	    constrainX(g, nlist, (int)nnodes, intersectY0, 1);
+	    constrainX(g, nlist, nnodes, intersectY0, 1);
 	    constrainY(g, nlist, (int)nnodes, intersectX, 1);
 	    break;
 	case AM_ORTHO_YX :
 	    constrainY(g, nlist, (int)nnodes, intersectX0, 1);
-	    constrainX(g, nlist, (int)nnodes, intersectY, 1);
+	    constrainX(g, nlist, nnodes, intersectY, 1);
 	    break;
 	case AM_PORTHOXY:
-	    constrainX(g, nlist, (int)nnodes, intersectY, 0);
+	    constrainX(g, nlist, nnodes, intersectY, 0);
 	    constrainY(g, nlist, (int)nnodes, intersectX, 0);
 	    break;
 	case AM_PORTHOYX:
 	    constrainY(g, nlist, (int)nnodes, intersectX, 0);
-	    constrainX(g, nlist, (int)nnodes, intersectY, 0);
+	    constrainX(g, nlist, nnodes, intersectY, 0);
 	    break;
 	case AM_PORTHO_YX :
 	    constrainY(g, nlist, (int)nnodes, intersectX0, 0);
-	    constrainX(g, nlist, (int)nnodes, intersectY, 0);
+	    constrainX(g, nlist, nnodes, intersectY, 0);
 	    break;
 	case AM_PORTHO :
 	default :
-	    constrainX(g, nlist, (int)nnodes, intersectY0, 0);
+	    constrainX(g, nlist, nnodes, intersectY0, 0);
 	    constrainY(g, nlist, (int)nnodes, intersectX, 0);
 	    break;
 	}
