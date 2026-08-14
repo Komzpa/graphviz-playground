cccd767783aee000ee3313a2f4ff9e030a368b4e
Matthew Fernandez <matthew.fernandez@gmail.com>
2026-08-14 16:58:49 -0700

    gvpr travDFS: reduce scope of 'curn'

--- lib/gvpr/gvpr.c
+++ lib/gvpr/gvpr.c
@@ -592,7 +592,6 @@ static void travDFS(Gpr_t *state, Expr_t *prog, comp_block *xprog,
                     trav_fns *fns) {
   Agnode_t *n;
   LIST(Agedge_t *) stk = {0};
-  Agnode_t *curn;
   Agedge_t *entry;
   int more;
   ndata *nd;
@@ -609,7 +608,7 @@ static void travDFS(Gpr_t *state, Expr_t *prog, comp_block *xprog,
     }
     seed.out.node = n;
     seed.in.node = 0;
-    curn = n;
+    Agnode_t *curn = n;
     entry = &seed.out;
     state->tvedge = NULL;
     MARK(nd);
