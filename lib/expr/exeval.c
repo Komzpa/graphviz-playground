ed66f26c34f7cc0ce0031f9f660b993bf32d7614
Matthew Fernandez <matthew.fernandez@gmail.com>
2026-08-27 17:30:17 -0700

    expr eval: fix unrecognized failure of 'fseek'/'rewind'

--- lib/expr/exeval.c
+++ lib/expr/exeval.c
@@ -1398,12 +1398,11 @@ static Extype_t eval(Expr_t *ex, Exnode_t *exnode, void *env) {
 		}
 		print(ex, exnode, env, buffer);
 		const int64_t size = gv_ftell(buffer);
-		if (size < 0) {
+		if (size < 0 || fseek(buffer, 0, SEEK_SET) < 0) {
 			fclose(buffer);
 			fprintf(stderr, "failed to read back temporary file\n");
 			graphviz_exit(EXIT_FAILURE);
 		}
-		rewind(buffer);
 		v.string = gv_arena_alloc(&ex->ve, 1, (size_t)size + 1);
 		if (fread(v.string, (size_t)size, 1, buffer) < 1) {
 			fprintf(stderr, "failed to read back temporary file\n");
