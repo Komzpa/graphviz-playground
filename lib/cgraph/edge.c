f75fa706b7690eb27124e45718c374e34b84b7a6
Matthew Fernandez <matthew.fernandez@gmail.com>
2026-09-02 17:01:38 -0700

    cgraph agfindedge_by_key: abbreviate initialization of lookup key
    This avoids having to think about parts of this structure that were
    previously left uninitialized as well as making it clearer this value is
    no longer needed after the call to `dtsearch`.

--- lib/cgraph/edge.c
+++ lib/cgraph/edge.c
@@ -119,18 +119,18 @@ Agedge_t *agnxtedge(Agraph_t * g, Agedge_t * e, Agnode_t * n)
 static Agedge_t *agfindedge_by_key(Agraph_t * g, Agnode_t * t, Agnode_t * h,
 			    Agtag_t key)
 {
-    Agedge_t *e, template;
+    Agedge_t *e;
     Agsubnode_t *sn;
 
     if (t == NULL || h == NULL)
 	return NULL;
-    template.base.tag = key;
-    template.node = t;		/* guess that fan-in < fan-out */
+    // guess that fan-in < fan-out, and thus searching `h`’s edges for `t` is
+    // cheaper than searching `t`’s edges for `h`
     sn = agsubrep(g, h);
     if (!sn) e = 0;
     else {
 	    dtrestore(g->e_id, sn->in_id);
-	    e = dtsearch(g->e_id, &template);
+	    e = dtsearch(g->e_id, &((Agedge_t){.base = {.tag = key}, .node = t}));
 	    sn->in_id = dtextract(g->e_id);
     }
     return e;
